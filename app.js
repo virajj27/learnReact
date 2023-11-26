779962402494c7f0f03778314ce8565d11f7b653
viraj padale <vpadale86@gmail.com>
2023-11-26 12:06:09 +0530

    episode 3

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//createElement(is an object) => it is converted to h1 tag via the render method
-//<div id="parent">
-//  <div id="child">
-//      <h1>welcome to react</h1>
-//      <h2>lets get started</h2>
-//  </div>
-// </div>
+const heading = React.createElement("h1", {}, "react code using createElement");
+const jsxheading = <h1>react code using jsx syntax!</h1>; //a react element
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" }, //these are props basically attributes + children prop(remember we used it in core concepts project)
-  "Hello world from viraj!"
-);
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "welcome to react"),
-    React.createElement("h2", {}, "lets get started"),
-  ])
-);
-console.log(parent); //an object
+// const fn=()=>true   === const fn=()=>{return true} KEEP NOTE OF THIS
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); //any code in html under root id will get replaced by the parent and not appended on the screen
+
+//functional components
+const Title = () => <h1>this is the title</h1>;
+
+const FunctionalHeader = () => (
+  <div>
+    {/* {Title()}  */}
+    <Title />
+    <h1 className="heading">react code using functional component</h1>
+  </div>
+);
+// root.render(heading);
+//root.render(<FunctionalHeader/>) this is how babel understands this syntax is to be rendered
+root.render(<FunctionalHeader />); //replacing the code inside root div in html
